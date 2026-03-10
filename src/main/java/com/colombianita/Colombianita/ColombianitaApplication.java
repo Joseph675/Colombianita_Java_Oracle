f207e7ab9d614d7c39c04c8a851e21621bdaf4b5
Kevin Mijares <50600961+Joseph675@users.noreply.github.com>
2026-03-10 00:33:08 -0500

    Primeros Cambios

---
+++ src/main/java/com/colombianita/Colombianita/ColombianitaApplication.java
@@ -0,0 +1,13 @@
+package com.colombianita.Colombianita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColombianitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColombianitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/colombianita/Colombianita/ColombianitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colombianita.Colombianita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColombianitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
